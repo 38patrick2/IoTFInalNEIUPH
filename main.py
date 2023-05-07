79dfd186567f82f27dcd75feaaed92b997fdfdf3
patri <patrick.poinar@qubuilder.com>
2023-05-07 15:51:34 -0500

    add 7digit button

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ client.loop_start()
 client.subscribe("testtopic/1")
 time.sleep(2)
 
-
-
-
 # Function to get current temperature from Raspberry Pi
 def get_current_temp():
    # Code to get current temperature from Raspberry Pi and publish to MQTT topic
@@ -30,7 +27,6 @@ def get_current_temp():
    # Publish temperature to topic "temp/current"
    # client.publish("temp/current", "25.5")
    client.publish("testtopic/1", "200")
-
 
 # Function to display average temperature based on list of prior temperatures
 def display_avg_temp():
@@ -40,12 +36,12 @@ def display_avg_temp():
    else:
        temp_label.config(text="Temperature list is empty")
 
-
 # Function to clear temperature list
 def clear_temps():
    temperatures.clear()
    temp_label.config(text="Temperature list cleared")
-
+def display7s():
+    pass
 # Create tkinter window and widgets
 window = tk.Tk()
 window.title("Temperature Monitor")
@@ -55,6 +51,7 @@ window.title("Temperature Monitor")
 get_temp_button = tk.Button(window, text="Get current temperature", command=get_current_temp)
 avg_temp_button = tk.Button(window, text="Display average temperature", command=display_avg_temp)
 clear_button = tk.Button(window, text="Clear temperature list", command=clear_temps)
+display7s_button = tk.Button(window, text="Display x on the Seven-segment", command=display7s)
 
 
 # Create label to display temperature information
@@ -65,7 +62,9 @@ temp_label = tk.Label(window, text="")
 get_temp_button.pack(pady=10)
 avg_temp_button.pack(pady=10)
 clear_button.pack(pady=10)
+display7s_button.pack(pady=10)
 temp_label.pack(pady=10)
+
 
 
 # Start tkinter event loop
